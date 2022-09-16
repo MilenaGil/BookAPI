d2d1933a97c60c3fb4d98e1137d602717f7d6399
MilenaGil <72659265+MilenaGil@users.noreply.github.com>
2022-09-16 10:19:43 +0200

    Add files via upload

---
+++ BookAPI/BooksDataStore.cs
@@ -0,0 +1,79 @@
+﻿using BookAPI.Models;
+
+namespace BookAPI
+{
+    public class BooksDataStore
+    {
+        public List<BookDto> Books { get; set; }
+        public static BooksDataStore Current { get; } = new BooksDataStore();
+        public BooksDataStore()
+        {
+            Books = new List<BookDto>()
+            {
+                new BookDto()
+                {
+                    Id = 1,
+                    Title = "Tytul1",
+                    Description = "Cos o ksiazce tytul1",
+                    Heroes = new List<HeroDto>()
+                    {
+                        new HeroDto()
+                        {
+                            Id=1,
+                            Name="Bohater1_ksiazki1",
+                            Description="Mily"
+                        },
+                        new HeroDto()
+                        {
+                            Id=2,
+                            Name="Bohater2_ksiazki1",
+                            Description="Szarmancka"
+                        }
+                    }
+                },
+                new BookDto()
+                {
+                    Id = 2,
+                    Title = "Tytul2",
+                    Description = "Wiele o tytul2",
+                    Heroes = new List<HeroDto>()
+                    {
+                        new HeroDto()
+                        {
+                            Id=3,
+                            Name="Bohater3_ksiazki2",
+                            Description="Wredna"
+                        },
+                        new HeroDto()
+                        {
+                            Id=4,
+                            Name="Bohater4_ksiazki2",
+                            Description="Pochmurna"
+                        }
+                    }
+                },
+                new BookDto()
+                {
+                    Id = 3,
+                    Title = "Tytul3",
+                    Description = "Notatka do tytul3",
+                    Heroes = new List<HeroDto>()
+                    {
+                        new HeroDto()
+                        {
+                            Id=5,
+                            Name="Bohater5_ksiazki3",
+                            Description="Bajkowy"
+                        },
+                        new HeroDto()
+                        {
+                            Id=6,
+                            Name="Bohater6_ksiazki3",
+                            Description="Potezna"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
